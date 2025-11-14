8688536a3aacfcddd47c24485314fcd2ad8cf7a9
valenvii <valentina.villarroel@est.fi.uncoma.edu.ar>
2025-11-13 22:52:43 -0300

    Creando estilos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = ({}) => {
+    return (
+        <div className="min-h-screen">
+        
+            {/* Theme Toggle */}
+
+            {/* Background Effects*/}
+
+            {/* Navbar */}
+
+            {/* Main Content */}
+
+            {/* Footer */}
+
+        </div>
+    )
+}

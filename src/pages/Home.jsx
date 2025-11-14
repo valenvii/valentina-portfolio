90527fda2b1a8a34c907b26cbc98ebc818a1288e
valenvii <valentina.villarroel@est.fi.uncoma.edu.ar>
2025-11-13 23:48:57 -0300

    Agrego funcionalidad para modo claro/oscuro usando el componente ThemeToggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
 export const Home = ({}) => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         
             {/* Theme Toggle */}
-
+                <ThemeToggle />
             {/* Background Effects*/}
 
             {/* Navbar */}

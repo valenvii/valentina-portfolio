1c652951bf034876b6b7980be35cca5f34f137c9
valenvii <valentina.villarroel@est.fi.uncoma.edu.ar>
2025-11-15 00:23:51 -0300

    Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { HeroSection } from "../components/HeroSection"
 import { Navbar } from "../components/Navbar"
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "@/components/StarBackground"
@@ -13,7 +14,9 @@ export const Home = ({}) => {
             {/* Navbar */}
             <Navbar />
             {/* Main Content */}
-
+            <main>
+                <HeroSection />
+            </main>
             {/* Footer */}
 
         </div>

446cfc4bf7db676d306b8612ac60433fdf3786bb
valenvii <valentina.villarroel@est.fi.uncoma.edu.ar>
2025-11-14 23:19:54 -0300

    Agrego fondo con estrellas y meteoritos, usando el componente StarBackground

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "@/components/StarBackground"
 
 export const Home = ({}) => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         
             {/* Theme Toggle */}
-                <ThemeToggle />
+            <ThemeToggle />
             {/* Background Effects*/}
-
+            <StarBackground />
             {/* Navbar */}
 
             {/* Main Content */}

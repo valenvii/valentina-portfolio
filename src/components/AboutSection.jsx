c4e70d95a629a6e870513d1e21eb413159b1b2f9
valenvii <valentina.villarroel@est.fi.uncoma.edu.ar>
2025-11-23 19:26:58 -0300

    Info herosection y aboutme

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Code, User } from "lucide-react"
+import { Briefcase, Code, GraduationCap, School } from "lucide-react"
 import { useTranslation } from 'react-i18next';
 
 export const AboutSection = () => {
@@ -43,7 +43,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Code className="h-6 w-6 text-primary" />
+                                    <GraduationCap className="h-6 w-6 text-primary" />
                                 </div>
                                 <div className="text-left">
                                     <h4 className="font-semibold text-lg"> {t('web_development')} </h4>
@@ -57,7 +57,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <User className="h-6 w-6 text-primary" />
+                                    <School className="h-6 w-6 text-primary" />
                                 </div>
                                  <div className="text-left">
                                     <h4 className="font-semibold text-lg"> {t('design')} </h4>
@@ -74,7 +74,7 @@ export const AboutSection = () => {
                                     <Briefcase className="h-6 w-6 text-primary" />
                                 </div>
                                  <div className="text-left">
-                                    <h4 className="font-semibold text-lg"> {t('projects')}  </h4>
+                                    <h4 className="font-semibold text-lg"> {t('work')}  </h4>
                                     <p className="text-muted-foreground">
                                         {t('projects_info')}
                                     </p>
